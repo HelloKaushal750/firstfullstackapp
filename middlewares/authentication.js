da750b6b69c7e0b21f7e6237d4f04c7f7ab0035c
HelloKaushal750 <kaushalv079@gmail.com>
2023-06-25 12:47:20 +0530

    response

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken')
 
 const authenticate = (req, res, next) => {
   if (!req.headers.authorization) {
-    return res.send("Please login again");
+    return res.josn({message:"Please login again"});
   }
   const token = req.headers.authorization.split(" ")[1];
   jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
     if(err){
-        res.send("Please Login")
+        res.send({message:"Please Login"})
     }else{
         req.body.userId = decoded.userId
         next()
