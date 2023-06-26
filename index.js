c2ebaeccc352b322c5797156cef60c0d26430ed0
HelloKaushal750 <kaushalv079@gmail.com>
2023-06-26 10:37:10 +0530

    cors

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const cors = require('cors')
 //hello
 const app = express();
 require('dotenv').config()
+app.use(cors())
 
 app.use(express.json())
-
-app.use(cors())
 
 app.get("/",(req,res)=>{
     res.send("Home Page")
